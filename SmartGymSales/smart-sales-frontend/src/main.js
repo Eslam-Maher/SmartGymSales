aa36ce1c82f98fad667383eb192947c3fa884c94
eslam.maher <eslam.maher@valeo>
2019-11-13 19:01:04 +0200

    change to bv

--- SmartGymSales/smart-sales-frontend/src/main.js
+++ SmartGymSales/smart-sales-frontend/src/main.js
@@ -6,18 +6,30 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import Vuex from 'vuex'
-import Vuesax from 'vuesax'
-import 'vuesax/dist/vuesax.css'
+import mixin from "./mixin.js";
+import { store } from "./store";
+import BootstrapVue from "bootstrap-vue";
+import Multiselect from "vue-multiselect"
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import vueDatepicker from "vuejs-datepicker";
+
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
+Vue.mixin(mixin);
+Vue.use(BootstrapVue);
 Vue.use(require('vue-moment'))
+Vue.component("multiselect", Multiselect);
+Vue.component("vue-datepicker", vueDatepicker);
+// Vue.component("page-header", () =>
+//   import("@/components/shared/PageHeader.vue")
+// );//to be handled later
 Vue.use(Vuex)
-Vue.use(Vuesax)
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
