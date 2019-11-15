e1d2a5a8d84ea145663fd92af24447fb4e493113
eslam.maher <eslam.maher@valeo>
2019-11-16 01:33:03 +0200

    users view

---
+++ SmartGymSales/smart-sales-frontend/src/views/Users.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <add-users></add-users>
+    <br/>
+    <users-grid></users-grid>
+  </div>
+</template>
+
+<script>
+import addUsers from "../components/Users/AddUsers";
+import usersGrid from "../components/Users/UsersGrid";
+export default {
+  name: "Users",
+  components: { usersGrid, addUsers }
+};
+</script>
+
+<style scoped></style>
