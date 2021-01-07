5e884dc7235f4b9a0e59f761fb984888dc157917
Eslam <eslam9369@gmail.com>
2021-01-07 23:26:06 +0200

    add home background

--- SmartGymSales/smart-sales-frontend/src/views/Home.vue
+++ SmartGymSales/smart-sales-frontend/src/views/Home.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Smart Body Gym logo" src="../assets/home.jpg" />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  },
-  created:function() {
-    if(!this.user ||!this.user.userRoles){
-    this.$router.push({ name: "Login" });
-    }
-  }
+
+  // created:function() {
+  //   if(!this.user ||!this.user.userRoles){
+  //   this.$router.push({ name: "Login" });
+  //   }
+  // }
 };
 </script>
